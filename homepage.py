94d00691104c4caa04fd1566e1f6caa6133fd2ee
pmerlin13 <manuel7_80@hotmail.com>
2024-08-28 00:14:15 -0400

    eliminar botones explicitos de login y usar st.registerv16

--- homepage.py
+++ homepage.py
@@ -134,7 +134,6 @@ def pagina_seleccion():
     st.title("Bienvenido")
     opcion = st.radio('Seleccione una opción', ['Login', 'Registro'])
     credentials = obtener_usuarios_y_contrasenas()
-    st.write(credentials)
     # Inicializar el autenticador de Streamlit
     authenticator = stauth.Authenticate(
         credentials,
